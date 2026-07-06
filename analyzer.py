9a909543d5b7a245815602207890f529ce1cd785
Reza-ashrafi <115076971+Reza-ashrafi@users.noreply.github.com>
2026-07-06 11:30:05 +0330

    Update analyzer.py

--- analyzer.py
+++ analyzer.py
@@ -1,11 +1,10 @@
-
 def analyze():
     return {
         "silver": 28.5,
         "usd": 60000,
-        "intrinsic": 0,
-        "market": 0,
-        "bubble": 0,
-        "score": 0,
-        "decision": "TEST MODE OK"
+        "intrinsic": 1000,
+        "market": 1020,
+        "bubble": 2,
+        "score": 95,
+        "decision": "🟢 TEST MODE CLEAN - NO API"
     }
